ff6c0f15da7fab063aa27eef596332f76590b952
Jared Silva <jaredsilva77@gmail.com>
2024-10-09 20:33:23 -0500

    Project 2 Module 13

---
+++ ETLPipeline/Resources/schema.sql
@@ -0,0 +1,62 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Category" (
+    "category" name   NOT NULL,
+    "category_id" name   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory" name   NOT NULL,
+    "subcategory_id" name   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" name   NOT NULL,
+    "description" name   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" flaot   NOT NULL,
+    "outcome" name   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" name   NOT NULL,
+    "currency" name   NOT NULL,
+    "launch_date" timestamp   NOT NULL,
+    "end_date" timestamp   NOT NULL,
+    "staff_pick" boolean   NOT NULL,
+    "spotlight" boolean   NOT NULL,
+    "category_and_sub-category" name   NOT NULL,
+    "category_id" name   NOT NULL,
+    "subcategory_id" name   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Contact" (
+    "contact_id" int   NOT NULL,
+    "first_name" name   NOT NULL,
+    "last_name" name   NOT NULL,
+    "email" name   NOT NULL
+);
+
+ALTER TABLE "Category" ADD CONSTRAINT "fk_Category_category_id" FOREIGN KEY("category_id")
+REFERENCES "Campaign" ("category_id");
+
+ALTER TABLE "Subcategory" ADD CONSTRAINT "fk_Subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Campaign" ("subcategory_id");
+
+ALTER TABLE "Contact" ADD CONSTRAINT "fk_Contact_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Campaign" ("contact_id");
+
---
+++ README.md
@@ -0,0 +1,27 @@
+# Crowdfunding Data Analysis Project
+
+## Overview
+This project focuses on building an ETL pipeline for crowdfunding data. This includes extracting data from Excel, transforming it for analysis, and loading it into a SQL database. The project demonstrates data manipulation, categorization, and preparation for detailed analytics on crowdfunding campaigns.
+
+## Group Project Members
+- Samson
+- Jared
+- Paul
+
+## Files Included
+- **ETL_Mini_Project_Starter_Code.ipynb**: Notebook for the ETL process.
+- **category.csv**: Categories derived from campaign data.
+- **subcategory.csv**: Subcategories derived from campaign data.
+- **campaign.csv**: Compiled campaign data.
+- **contacts.csv**: Contact information related to campaigns.
+- **schema.sql**: The SQL schema used to create the database.
+- **schema diagram.png**: A diagram illustrating the database structure and relationships.
+
+## Database
+- **Schema and Setup**: Database schema provided in SQL format to set up the tables.
+- **Data Upload**: Data from CSV files uploaded to respective tables in the database.
+
+## Importing Schema
+1. **Run the SQL schema**: Execute the `schema.sql` file in PostgreSQL to create the database schema.
+2. **Import the CSV files**: Import each CSV file into the appropriate PostgreSQL table, with `campaign.csv` being imported last, as it depends on data from other tables.
+
